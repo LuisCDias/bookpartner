9691e3e52473d2fab2ece8d6e45c9aaf3931e86f
Luis Dias <lcdias127@gmail.com>
2013-05-21 13:22:43 +0100

    add de coisas que faltaram

---
+++ bookpartner/src/fe/up/pt/partner/GenreBooksActivity.java
@@ -0,0 +1,155 @@
+package fe.up.pt.partner;
+
+import oauth2.OAuthAccessTokenActivity;
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Camera.Size;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+import fe.up.pt.partner.R;
+import fragments.GenreBooks_Results;
+import fragments.MainFragment;
+import fragments.MainFragment_Genres;
+import fragments.MainFragment_Recent;
+import fragments.MainFragment_Top;
+
+public class GenreBooksActivity extends SherlockFragmentActivity implements TabListener {
+
+	ViewPager mViewPager;
+	ActionBar mActionBar;
+	TabsAdapter mTabsAdapter;
+	String userToken;
+	String useMode ;
+	String userID;
+	String userRating;
+	String userName;
+	String title;
+	Bundle extras;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		extras = getIntent().getExtras();
+		
+		if(extras!=null)
+			title = extras.getString("genre");
+		
+		setTheme(R.style.Theme_Sherlock);
+		super.onCreate(savedInstanceState);		
+
+		mViewPager = new ViewPager(this);
+		mViewPager.setId(R.id.pager);
+		mViewPager.setBackgroundColor(Color.BLACK);
+		setContentView(mViewPager);
+
+		mActionBar = getSupportActionBar();
+		mActionBar.setDisplayShowTitleEnabled(true);
+		mActionBar.setHomeButtonEnabled(true);
+		mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		mTabsAdapter = new TabsAdapter(this, mViewPager);
+		mTabsAdapter.addTab(mActionBar.newTab().setText("Books on '"+title+"'"),
+				GenreBooks_Results.GenreBooks_Results_Aux.class, null);
+
+
+	}	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
+		MenuItem LogInItemMenu = menu.findItem(R.id.menu_login);
+		
+		LogInItemMenu.setTitle(PartnerAPI.Strings.LOGIN);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// TODO handle clicking the app icon/logo
+			return false;
+		case R.id.menu_search:
+			//startSearch(title, false,null,false);
+			onSearchRequested();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+
+	@Override
+	public boolean onSearchRequested() {
+		SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+
+		if(searchManager!=null)
+		{
+			
+			// start the search with the appropriate searchable activity
+			// so we get the correct search hint in the search dialog
+			Bundle b = new Bundle();
+
+			searchManager.startSearch(null, false,new ComponentName(this, SearchableActivity.class), b, false);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	protected void onStop() {
+		setResult(0);
+		super.onStop();
+	}
+	@Override
+	protected void onDestroy() {
+		setResult(0);
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(resultCode==3){
+			finish();
+		}
+	}
+
+	
+	/*public void openFullCover(View v){
+		
+		MainFragment_Recent.openFullCover(v, this);
+	}*/
+
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+
+	}
+
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+
+
+	}
+
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+
+
+	}
+}
---
+++ bookpartner/src/fragments/GenreBooks_Results.java
@@ -0,0 +1,214 @@
+package fragments;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import AsyncTasks.ResponseCommand;
+import AsyncTasks.ResponseCommand.ERROR_TYPE;
+import ListAdapter.ListAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import fe.up.pt.partner.BooksPanelActivity;
+import fe.up.pt.partner.FullCoverActivity;
+import fe.up.pt.partner.PartnerAPI;
+import fe.up.pt.partner.R;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.app.SherlockListFragment;
+
+
+public class GenreBooks_Results extends SherlockFragmentActivity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+
+		// Create the list fragment and add it as our sole content.
+		if (getSupportFragmentManager().findFragmentById(android.R.id.content) == null) {
+			GenreBooks_Results_Aux list = new GenreBooks_Results_Aux();
+			getSupportFragmentManager().beginTransaction()
+			.add(android.R.id.content, list).commit();
+		}
+
+	}
+
+	public static class GenreBooks_Results_Aux extends SherlockListFragment {
+
+		private ArrayList<String> titles;
+		private ArrayList<String> ids;
+		private ArrayList<String> authors;
+		private ArrayList<String> page_counts;
+		private ArrayList<String> ratings;
+		private ArrayList<String> covers;
+		private ArrayList<String> descriptions;
+
+
+		private void searchIt(String URL) {
+			Log.d("url", URL);
+			getActivity().getWindow().setSoftInputMode(
+					WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+			PartnerAPI.requestURL(URL, new ResponseCommand() {
+				
+				public void onResultReceived(Object... results) {
+
+					Log.d("json", results[0].toString());
+					JSONObject book = (JSONObject) results[0];
+
+					authors = new ArrayList<String>();
+					titles = new ArrayList<String>();
+					ids = new ArrayList<String>();
+					page_counts = new ArrayList<String>();
+					ratings = new ArrayList<String>();
+					covers = new ArrayList<String>();
+					descriptions = new ArrayList<String>();
+
+					int i = 0;
+
+					try {
+						
+						JSONArray items = book.getJSONArray("items");
+						while (!items.isNull(i)) {
+
+							JSONObject item = items.getJSONObject(i);
+							
+							ids.add(item.getString("id"));
+							JSONObject volumeInfo = item.getJSONObject("volumeInfo");
+							titles.add(volumeInfo.getString("title"));
+							
+							JSONArray authors_array = null;
+							
+							if(volumeInfo.has("authors")){
+								authors_array = volumeInfo.getJSONArray("authors");
+								/*mais do que um author? tratar no webservice, para já placeholder com o primeiro encontrado*/
+								authors.add(authors_array.get(0).toString());
+							}
+							else
+								authors.add(PartnerAPI.Strings.NO_AUTHOR_AVAILABLE);
+									
+							
+							if(volumeInfo.has("pageCount"))
+								page_counts.add(volumeInfo.getString("pageCount"));
+							else
+								page_counts.add("N/A");
+							if(volumeInfo.has("averageRating"))
+								ratings.add(volumeInfo.getString("averageRating"));
+							else
+								ratings.add(PartnerAPI.Strings.NO_RATING_AVAILABLE);
+							
+							/* Para evitar o facto de poder vir com uma descrição vazia,
+							 * ou não ter.*/
+							if(volumeInfo.has("description"))
+								descriptions.add(volumeInfo.getString("description"));
+							else
+								descriptions.add(PartnerAPI.Strings.NO_DESCRIPTION_AVAILABLE);
+							
+							JSONObject image_links = null;
+							if(volumeInfo.has("imageLinks"))
+								image_links = volumeInfo.getJSONObject("imageLinks");
+							
+							if(image_links != null){
+							
+								if(image_links.has("thumbnail")) 
+									covers.add(image_links.getString("thumbnail"));
+								else
+									covers.add(PartnerAPI.Strings.NO_COVER_AVAILABLE);
+							}
+							else
+								covers.add(PartnerAPI.Strings.NO_COVER_AVAILABLE);
+							
+							i++;
+						}
+						
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+
+					
+					setListAdapter(new ListAdapter(getActivity(), titles, ids, authors, ratings, page_counts, covers));
+
+				}
+
+				@Override
+				public void onError(ERROR_TYPE error) {
+					
+					if(error.toString().equals(ERROR_TYPE.NETWORK))
+						Toast.makeText(getActivity(), PartnerAPI.Strings.SERVER_CONNECTION,
+								Toast.LENGTH_LONG).show();
+					else if(error.toString().equals(ERROR_TYPE.GENERAL))
+						Toast.makeText(getActivity(), PartnerAPI.Strings.CHECK_CONNECTION,
+								Toast.LENGTH_LONG).show();
+				}
+			});
+			// }
+	}
+
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			super.onActivityCreated(savedInstanceState);
+
+			// remove divider
+			this.getListView().setDividerHeight(0);
+
+			//AsyncTasks to search something
+			//searchIt("https://www.googleapis.com/books/v1/volumes?q=a+song+of+ice+and+fire&key="+PartnerAPI.APIkeys.GOOGLE_BOOKS_KEY);
+
+
+		}
+
+		@Override
+		public void onListItemClick(ListView l, View v, int position, long id) {
+			
+			String id_book = ids.get(position);
+			String title_book = titles.get(position);
+			String author_book = authors.get(position);
+			String pages_book = page_counts.get(position);
+			String rating_book = ratings.get(position);
+			String cover_book = covers.get(position);
+			String description = descriptions.get(position);
+			
+			Intent intent = new Intent(this.getSherlockActivity(), BooksPanelActivity.class );
+
+			intent.putExtra("id", id_book);
+			intent.putExtra("title", title_book);
+			intent.putExtra("author", author_book);
+			intent.putExtra("page_count", pages_book);
+			intent.putExtra("rating", rating_book);
+			intent.putExtra("cover", cover_book);
+			intent.putExtra("description", description);
+			
+			//include the user id
+			//intent.putExtra(PartnerAPI.Strings.USE_MODE_BUNDLE, useMode);
+			
+			startActivity(intent);	
+
+		}
+
+	}
+	/*public static void openFullCover(View v, Context ctx){
+		
+		switch(v.getId()){
+		
+		case R.id.book_cover:
+			Log.d("BOOK", "COVER");
+			
+			Intent intent = new Intent(ctx, FullCoverActivity.class);
+			v.get
+			break;
+	
+		}
+	}*/
+}
